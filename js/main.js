767ab94f6a50fe657016cff09ea4c31aa9523754
dunkerbunker <76224462+dunkerbunker@users.noreply.github.com>
2022-01-29 12:26:13 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,75 @@
+
+/*----------------- navigation menu --------------------*/
+
+(() =>{
+  
+  const hamburgerBtn = document.querySelector(".hamburger-btn"),
+  navMenu = document.querySelector(".nav-menu"),
+  closeNavBtn = navMenu.querySelector(".close-nav-menu");
+
+  hamburgerBtn.addEventListener("click", showNavMenu);
+  closeNavBtn.addEventListener("click", hideNavMenu);
+
+  function showNavMenu(){
+    navMenu.classList.add("open");
+    bodyScrollingToggle();
+  }
+  function hideNavMenu(){
+    navMenu.classList.remove("open");
+    fadeOutEffect();
+    bodyScrollingToggle();
+  }
+  function fadeOutEffect(){
+  	document.querySelector(".fade-out-effect").classList.add("active");
+  	setTimeout(() =>{
+      document.querySelector(".fade-out-effect").classList.remove("active");
+  	},300)
+  }
+
+})();
+
+function bodyScrollingToggle(){
+  document.body.classList.toggle("hidden-scrolling");
+}
+
+/*--------------------- modal --------------------------*/
+
+(() =>{
+  const openModalButtons = document.querySelectorAll('[data-modal-target]')
+  const closeModalButtons = document.querySelectorAll('[data-close-button]')
+  const overlay = document.getElementById('overlay')
+
+  openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(button.dataset.modalTarget)
+      openModal(modal)
+    })
+  })
+
+  overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+      closeModal(modal)
+    })
+  })
+
+  closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.modal')
+      closeModal(modal)
+    })
+  })
+
+  function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+  }
+
+  function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+  }
+})();
+
